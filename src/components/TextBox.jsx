2683786f4155517751d236be67da15ea104b3c77
shaharyar saleem <shaharyarsaleem71@gmail.com>
2022-03-07 21:30:21 +0100

    feat: clear txt added

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -15,13 +15,18 @@ export default function TextBox(props) {
   }
   const convertToCamelCase = () => {
        let camelcaseTxt = text.replace(/(?:^\w|[A-Z]|\b\w)/g, function(text, index) {
-        return index === 0 ? text.toLowerCase() : text.toUpperCase()
+        return index === true ? text.toLowerCase() : text.toUpperCase()
       })
       setText(camelcaseTxt)
   }
   const copyToClipboard = () => {
     navigator.clipboard.writeText(text)
     alert("Text Copied")
+  }
+  const clearText = () => {
+    const confirm = window.confirm("Are you sure to remove the text?")
+    console.log(confirm)
+    confirm ? setText("") : setText(text)
   }
   const updateTextValue = (event) => {
       setText(event.target.value)
@@ -32,10 +37,11 @@ export default function TextBox(props) {
       <div className="mb-3">
           <h1 className='my-4'>{props.heading}</h1>
           <textarea className="form-control mb-3" value={text} onChange={updateTextValue} rows="10"></textarea>
-          <button className="btn btn-primary btn-lg mr-4" onClick={convertToUpperCase}>Convert to Uppercase</button>
-          <button className="btn btn-success btn-lg mx-3" onClick={convertToLowerCase}>Convert to Lowercase</button>
+          <button className="btn btn-primary btn-lg mr-4" onClick={convertToUpperCase}>Convert to UpperCase</button>
+          <button className="btn btn-success btn-lg mx-3" onClick={convertToLowerCase}>Convert to LowerCase</button>
           <button className="btn btn-secondary btn-lg mx-2" onClick={convertToCamelCase}>Convert to CamelCase</button>
-          <button className="btn btn-danger btn-lg mx-2" onClick={copyToClipboard}>Copy the Text</button>
+          <button className="btn btn-success btn-lg mx-2" onClick={copyToClipboard}>Copy Text</button>
+          <button className="btn btn-danger btn-lg mx-2" onClick={clearText}>Clear Text</button>
        </div>
     </div>
     <div className="container">
