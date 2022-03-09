862d440d6567e239ea9097194c1b2e4670d0fce0
shaharyar saleem <shaharyarsaleem71@gmail.com>
2022-03-09 16:28:01 +0100

    feat: dark mode completed

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -31,12 +31,38 @@ export default function TextBox(props) {
   const updateTextValue = (event) => {
       setText(event.target.value)
   }
+
+  // styling
+  const [themeStyle, setThemeStyle] = useState({
+    backgroundColor: 'white',
+    color: 'black',
+  })
+  const [btnTxt, setBtnTxt] = useState("Enable Dark Mode")
+
+  const toggleStyle = ()=> {
+     if(themeStyle.backgroundColor == "white") {
+       setThemeStyle({
+         backgroundColor: "black",
+         color: "white",
+       })
+       setBtnTxt("Enable Light Mode")
+     }
+
+     else{
+       setThemeStyle({
+         backgroundColor: "white",
+         color: "black",
+       })
+       setBtnTxt("Enable Dark Mode")
+     }
+  }
   return (
       <>
-    <div className='container'>
+      <button className="btn btn-md btn-success float-right mt-3 mb-3" onClick={toggleStyle}>{btnTxt}</button>
+    <div className='container py-5' style={themeStyle}>
       <div className="mb-3">
           <h1 className='my-4'>{props.heading}</h1>
-          <textarea className="form-control mb-3" value={text} onChange={updateTextValue} rows="10"></textarea>
+          <textarea className="form-control mb-3" style={themeStyle} value={text} onChange={updateTextValue} rows="10"></textarea>
           <button className="btn btn-primary btn-lg mr-4" onClick={convertToUpperCase}>Convert to UpperCase</button>
           <button className="btn btn-success btn-lg mx-3" onClick={convertToLowerCase}>Convert to LowerCase</button>
           <button className="btn btn-secondary btn-lg mx-2" onClick={convertToCamelCase}>Convert to CamelCase</button>
@@ -44,9 +70,9 @@ export default function TextBox(props) {
           <button className="btn btn-danger btn-lg mx-2" onClick={clearText}>Clear Text</button>
        </div>
     </div>
-    <div className="container">
+    <div className="container py-4" style={themeStyle}>
         <h2>Your Text Summary</h2>
-        <div className="bg-light p-2">
+        <div className="p-2">
         <p><b>{text.split(' ').length}</b> <i>Words</i> AND <b>{text.length}</b>  <i>Characters</i></p>
         <p><b>{0.0032 * text.split(' ').length}</b> Minutes to read this text.</p>
         </div>
@@ -56,6 +82,7 @@ export default function TextBox(props) {
     </>
   );
 }
+
 
 TextBox.propTypes = {
     heading: propTypes.string.isRequired
